8d6ecc5ef807f41b26d640caa1d782895763f55e
harshgupta20 <hgreatgupta2001@gmail.com>
2021-02-16 23:42:19 +0530

    Awesome 😉😉, Our project is ready

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelector("#volume1-button").addEventListener("cilck", function(){
+  var audio = new Audio("images/web-icon.png");
+  audio.play();
+});
+
+document.querySelector("#volume2-button").addEventListener("cilck", function(){
+  var audio = new Audio("images/web-icon.png");
+  audio.play();
+});
